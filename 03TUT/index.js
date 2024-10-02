d406f5e1ca22611a3b87eea04e75322acdff3387
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-10-02 21:33:51 +0530

    tutorial 4 : event listeners

--- 03TUT/index.js
+++ 03TUT/index.js
@@ -1,112 +1,20 @@
-const {format} = require('date-fns');
-const { v4: uuid } = require('uuid'); //specific version
+const logEvents = require("./logEvents");
 
-console.log(format(new Date(),'yyyyMMdd\tHH:mm:ss'));
+//events common core modules
 
-console.log(uuid()); //logs diff id 
-/**
- * globally
- * */
-//cmd : npm i (install or add) nodemon (monitors the files and restarts the se4rver, no need to write 'node' everytime) intall in the project root::: sudo npm i nodemon -g
-//nodemon : looks out for index js file, else nodemon filename
-//resytarts whenever we change or make any issues
+//common core module event
+const EventEmitter = require('events');
 
-/**
- * 
- * locally to the project
- * 
- */
+class MyEmitter extends EventEmitter{}; //from the documentation
 
-//initialize npm : npm init
-//it creates package.json : this file will know what packages to install for the app(hosted and built, build commands just installs it), if we have github, using this file, we won't have to send as much data (as a node package has)
-//asks the following set of questions
-/***
- * 
- * 
- * 
-package name: (03tut) 
-version: (1.0.0) 
-description: 
-entry point: (index.js) 
-test command: 
-git repository: 
-keywords: 
-author: 
-license: (ISC) 
-About to write to /Applications/XAMPP/xamppfiles/htdocs/nodeJs/03TUT/package.json:
+//initialize the object
+const myEmitter = new MyEmitter();
 
-{
-  "name": "03tut",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "",
-  "license": "ISC"
-}
+//adding listerner for log event : on to add listener
+myEmitter.on('log' , (msg)=> logEvents(msg));
 
+//emit the event to test it out : emit to emitting the event
+setTimeout(()=>{
+    myEmitter.emit('log','Log Event emitted'); //we can add more than 1 perimeter
 
-Is this OK? (yes) */
-//download the package => sudo npm i date-fns
-/***
- * 
- * 
-this add dependencies to the package.json and adds node_module folders for the package too,
-and whenever the project is build , we will know this package is the part of the app
-production dependencies
-*/
-//to remove node modules from git uppload : we add .gitignore file
-//add the file to : root of the folder you are in
-
-//adding dev dependencies : for packages that are needed only during development not in prod
-/**
- * sudo npm i nodemon -D
- * 
- */
-
-//SCript : package.json 
-/**
- * types of scripts : start, dev, built and test
- * inside package.json : replace the script type and mention the command inside it
- * 
- * then run the cmd : npm run dev : it will run the cmd script from package.json, with "dev" being from "scripts" tag and running "index" file
- * 
- */
-
-//UUID: creates diff id for each entry
-/**
- * 
- * package.json has :  "uuid": "^10.0.0"
- * console logs : ede766bd-ca35-46ad-897f-f4ea9a88d17f
- */
-
-
-//versions in devdependencies and dependencies
-/**
- * 
- * inside package.json : "packageName" : "^10.1.2."
- * "10" : major version, "1" : minor version,  "2" : patch
- * "^" : updates the minor and patch only
- * "~" : update only the patch
- * "*" : update anything or everything
- * 
- */
-
-
-/**
- * list of cmd :
- * 
-(project root) => sudo npm i nodemon -g
-(folder root) => nodemon
-(initialize the npm) =< npm init
-(install locally) => sudo npm i date-fns
-(dev dependency install) => sudo npm i nodemon -D
-(for script-package json) => npm run dev
-(for installing package) => sudo npm i uuid
-(for specific package install) => sudo npm i nodemon@versionName
-(for updates) => sudo npm update : it checks for updates
-(for unintall) => npm rm packageName -D | -g : depending upon devdependencies or global : don't forget to check scripts in package jdon
-
-*/
+}, 2000);
---
+++ 03TUT/logEvents.js
@@ -0,0 +1,40 @@
+const {format} = require('date-fns');
+const { v4: uuid } = require('uuid'); //specific version
+
+//common core modules
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const logEvents = async (message) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem =   `${dateTime}\t${uuid()}\t${message}\t`;
+    console.log(logItem);
+    try{
+        if(!fs.existsSync(path.join(__dirname, 'logs'))){
+            await fsPromises.mkdir(path.join(__dirname,'logs'));
+        }
+        //test 
+        await fsPromises.appendFile(path.join(__dirname,'logs','eventLog.txt'), logItem);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = logEvents; //exporting log event function and using in index.js
+
+/**
+ * list of cmd :
+ * 
+(project root) => sudo npm i nodemon -g
+(folder root) => nodemon
+(initialize the npm) =< npm init
+(install locally) => sudo npm i date-fns
+(dev dependency install) => sudo npm i nodemon -D
+(for script-package json) => npm run dev
+(for installing package) => sudo npm i uuid
+(for specific package install) => sudo npm i nodemon@versionName
+(for updates) => sudo npm update : it checks for updates
+(for unintall) => npm rm packageName -D | -g : depending upon devdependencies or global : don't forget to check scripts in package jdon
+
+*/
