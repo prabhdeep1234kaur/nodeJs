c2c2f1ab9c414eb70291f2e276eb11b7732dc8c2
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-10-26 16:19:06 +0530

    USER authentication

--- 03TUT/controllers/refreshTokenController.js
+++ 03TUT/controllers/refreshTokenController.js
@@ -20,18 +20,21 @@ const handleRefreshToken = (req,res)=>{
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded)=>{
-            if(err || foundUser.username !== decoded.username){
-                return res.sendStatus(403);
-            } else {
-                const accessToken = jwt.sign(
-                    {
-                        "username": decoded.username
-                    },
-                    process.env.ACCESS_TOKEN_SECRET,
-                    {expiresIn: `30s`}
-                );
-                res.json({accessToken})
-            }
+            if(err || foundUser.username !== decoded.username)return res.sendStatus(403)
+            const roles = Object.values(foundUser.roles);
+            const accessToken = jwt.sign(
+                {
+                   "UserInfo" : {
+                    "username": decoded.username,
+                    "roles" : roles
+                   }
+                   
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                {expiresIn: `30s`}
+            );
+            res.json({accessToken})
+            
         }
     );
 }
